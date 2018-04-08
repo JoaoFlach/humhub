ac9c50fba71a5608f3d7b94370a5474c98cfd746
Joao Flach <joao.flachsilva@gmail.com>
2018-04-07 21:42:02 -0300

    Adding a controller to that makes the system request data from a Producer

--- protected/humhub/modules/api/controllers/ProducerController.php
+++ protected/humhub/modules/api/controllers/ProducerController.php
@@ -1,67 +1,80 @@
 <?php
+
 namespace humhub\modules\api\controllers;
 
 use Yii;
 use humhub\modules\api\controllers\BaseController;
 use humhub\modules\api\models\Producer;
+use Zend\Http\Client;
 
-class ProducerController extends BaseController
-{
+class ProducerController extends BaseController {
+
     public $modelClass = 'humhub\modules\api\models\Producer';
 
     /**
      * @inheritdoc
      */
-    public function actions()
-    {
+    public function actions() {
         $actions = parent::actions();
         unset($actions['index'], $actions['view'], $actions['delete'], $actions['update'], $actions['create']);
         return $actions;
     }
-    
-    public function actionIndex(){
+
+    public function actionIndex() {
         $query = Producer::find();
         return $query->all();
     }
-    
+
     public function actionCreate() {
         $producer = new Producer();
-        
+
         $producer->url = Yii::$app->request->getBodyParam('url');
         $producer->ip = Yii::$app->request->getBodyParam('ip');
         $producer->port = Yii::$app->request->getBodyParam('port');
         $producer->active = false;
-        
-        if($producer->save()){
+
+        if ($producer->save()) {
             return $producer;
         } else {
             throw new Exception("Could not save this item");
-        } 
+        }
     }
-    
+
     public function actionUpdate($id) {
         $producer = Producer::find()->where(['id' => $id])->one();
-        
+
         $producer->url = Yii::$app->request->getBodyParam('url');
         $producer->ip = Yii::$app->request->getBodyParam('ip');
         $producer->port = Yii::$app->request->getBodyParam('port');
-        
-        
-        if($producer->save()){
+
+
+        if ($producer->save()) {
             return $producer;
         } else {
             throw new Exception("Could not update this item");
         }
     }
-    
+
     public function actionView($id) {
         $producer = Producer::find()->where(['id' => $id])->one();
-        
+
         return $producer;
     }
 
-    public function actionAttributes(){
+    public function actionAttributes() {
         $producer = new Producer();
         return $producer->attributes();
     }
+
+    public function actionRequest($id) {
+        $producer = Producer::find()->where(['id' => $id])->one();
+        $url = $producer->url;
+
+        $client = new Client();
+        $client->setUri($url);
+        $response = $client->send();
+        $responsebody = $response->getBody();
+        return $responsebody;
+    }
+
 }
