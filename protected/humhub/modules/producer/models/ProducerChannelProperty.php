ad7ec7e594cd4465c08d4a6a83be03b172ce2144
Joao Flach <joao.flachsilva@gmail.com>
2018-05-22 22:35:54 -0300

    creating producer channel property model

---
+++ protected/humhub/modules/producer/models/ProducerChannelProperty.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace humhub\modules\producer\models;
+use Yii;
+use humhub\modules\producer\models\ProducerChannel;
+
+/**
+ * This is the model class for table "producer".
+
+ * @property integer $id
+ * @property string $channel_id
+ * @property string $property_name
+ * @property string $type
+ */
+class ProducerChannelProperty extends \yii\db\ActiveRecord  
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'producer_channel_property';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [            
+            
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+        ];
+    }    
+   
+    /**
+     * @return ProducerChannel
+     */
+    public function getChannel()
+    {
+        $channel = ProducerChannel::findOne(['id' => $this->channel_id]);
+
+        return $channel;
+    }
+   
+}
